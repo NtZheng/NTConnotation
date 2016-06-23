b9f20f1847ebedc5a2c2fc7bc4343e113c72afc0
郑祯 <2745674085@qq.com>
2016-06-24 02:28:08 +0800

    完成了APP的icon，LaunchImage等初步配置

---
+++ NTConnotation/NTConnotation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NTConnotation
+//
+//  Created by 郑祯 on 16/6/24.
+//  Copyright © 2016年 nineteen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NTConnotation/NTConnotation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NTConnotation
+//
+//  Created by 郑祯 on 16/6/24.
+//  Copyright © 2016年 nineteen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
