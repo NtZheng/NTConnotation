1f10e3f26fd2bb8721b82b3312700c626e62b53f
郑祯 <2745674085@qq.com>
2016-06-27 02:49:00 +0800

    将项目改为纯代码方式..

--- NTConnotation/NTConnotation/AppDelegate.m
+++ NTConnotation/NTConnotation/AppDelegate.m
@@ -16,7 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
