8e633206c1122c523a561533de108cb226128cbe
NtZheng <2745674085@qq.com>
2016-07-30 18:58:32 +0800

    自定义了一个UITextField类，用来定制本项目中UITextField的样式

---
+++ NTConnotation/NTConnotation/Main/Primary/View/NTTextField.h
@@ -0,0 +1,13 @@
+//
+//  NTTextField.h
+//  NTConnotation
+//
+//  Created by Nineteen on 7/30/16.
+//  Copyright © 2016 nineteen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTTextField : UITextField
+
+@end
---
+++ NTConnotation/NTConnotation/Main/Primary/View/NTTextField.m
@@ -0,0 +1,18 @@
+//
+//  NTTextField.m
+//  NTConnotation
+//
+//  Created by Nineteen on 7/30/16.
+//  Copyright © 2016 nineteen. All rights reserved.
+//
+
+#import "NTTextField.h"
+
+@implementation NTTextField
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    self.tintColor = [UIColor whiteColor];
+}
+
+@end
