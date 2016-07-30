4f51dc8dab4c9f17369afb4f4d857a1f7774e574
NtZheng <2745674085@qq.com>
2016-07-30 20:09:04 +0800

    完成了登录界面和注册界面的动画跳转

--- NTConnotation/NTConnotation/Main/Primary/Controller/NTLoginViewController.m
+++ NTConnotation/NTConnotation/Main/Primary/Controller/NTLoginViewController.m
@@ -10,6 +10,8 @@
 
 @interface NTLoginViewController ()
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *leftConstant;
+
 @end
 
 @implementation NTLoginViewController
@@ -23,6 +25,31 @@
 // 在iOS7之前是通过UIApplication来统一管理状态栏的，到了iOS7之后，苹果将管理安排到了每个控制器
 - (UIStatusBarStyle)preferredStatusBarStyle {
     return UIStatusBarStyleLightContent; // 将状态栏的文字变成白色
+}
+
+- (IBAction)backButtonAction:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:^{
+        
+    }];
+}
+
+- (IBAction)showLoginOrRegisterButtonAction:(UIButton *)button {
+    [self.view endEditing:YES];
+    if (self.leftConstant.constant == 0) {
+        self.leftConstant.constant = -NTScreenWidth;
+        button.selected = YES;
+    } else {
+        self.leftConstant.constant = 0;
+        button.selected = NO;
+    }
+    
+    [UIView animateWithDuration:1.0 animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
 }
 
 @end
