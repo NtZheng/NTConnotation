1a6f22e0545800ee9532efb99dd2e76a28867f7b
NtZheng <2745674085@qq.com>
2016-07-31 10:09:33 +0800

    完成了占位文字随着焦点获取的变化而改变颜色的效果..

--- NTConnotation/NTConnotation/Main/Primary/View/NTTextField.m
+++ NTConnotation/NTConnotation/Main/Primary/View/NTTextField.m
@@ -13,8 +13,20 @@
 - (void)awakeFromNib {
     [super awakeFromNib];
     self.tintColor = [UIColor whiteColor];
+    [self addTarget:self action:@selector(beginEditAction) forControlEvents:UIControlEventEditingDidBegin];
+    [self addTarget:self action:@selector(endEditAction) forControlEvents:UIControlEventEditingDidEnd];
+}
+
+#pragma mark - methods
+- (void)beginEditAction {
     NSMutableDictionary *attributeDictionary = [NSMutableDictionary dictionary];
     attributeDictionary[NSForegroundColorAttributeName] = [UIColor whiteColor];
+    self.attributedPlaceholder = [[NSAttributedString alloc]initWithString:self.placeholder attributes:attributeDictionary];
+}
+
+- (void)endEditAction {
+    NSMutableDictionary *attributeDictionary = [NSMutableDictionary dictionary];
+    attributeDictionary[NSForegroundColorAttributeName] = [UIColor grayColor];
     self.attributedPlaceholder = [[NSAttributedString alloc]initWithString:self.placeholder attributes:attributeDictionary];
 }
 
