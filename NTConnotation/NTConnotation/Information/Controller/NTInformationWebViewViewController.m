20d5e2b865d948a20e1dbde11cf686f151678996
NtZheng <2745674085@qq.com>
2016-08-13 12:45:41 +0800

    完成了网页加载界面中三个按钮的功能和显示效果

--- NTConnotation/NTConnotation/Information/Controller/NTInformationWebViewViewController.m
+++ NTConnotation/NTConnotation/Information/Controller/NTInformationWebViewViewController.m
@@ -8,9 +8,12 @@
 
 #import "NTInformationWebViewViewController.h"
 
-@interface NTInformationWebViewViewController ()
+@interface NTInformationWebViewViewController () <UIWebViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *backBarButtonItem;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *forwardBarButtonItem;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *refreshBarButtonItem;
 
 @end
 
@@ -19,6 +22,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:self.URL]]];
+}
+
+- (IBAction)backAction:(id)sender {
+    [self.webView goBack];
+}
+
+- (IBAction)forwardAction:(id)sender {
+    [self.webView goForward];
+}
+
+- (IBAction)refreshAction:(id)sender {
+    [self.webView reload];
+}
+
+#pragma mark - delegate
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    self.backBarButtonItem.enabled = self.webView.canGoBack;
+    self.forwardBarButtonItem.enabled = self.webView.canGoForward;
 }
 
 @end
